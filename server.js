9f2b41910400d2d3dcf3b652fe75e34bd0e723e7
Jonathan JG <jonysthil@gmail.com>
2023-08-05 18:26:39 -0600

    iniciando bot

---
+++ server.js
@@ -0,0 +1,28 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { 
+        small: true
+    });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+	console.log(message.body);
+});
+
+client.on('message', message => {
+	if(message.body === 'Hello') {
+		//message.reply('World');
+        client.sendMessage(message.from, 'World');
+	}
+});
+
+
+client.initialize();
