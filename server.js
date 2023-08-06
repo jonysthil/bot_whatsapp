0d1ebaf3a9c11f778f1bb2d57df981b2fc4ef6f3
Jonathan JG <jonysthil@gmail.com>
2023-08-05 20:02:13 -0600

    probando

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const qrcode = require('qrcode-terminal');
 
-const { Client } = require('whatsapp-web.js');
+const { Client, MessageMedia } = require('whatsapp-web.js');
 const client = new Client();
+
+const media = MessageMedia.fromFilePath('./img/bot.png');
 
 client.on('qr', qr => {
     qrcode.generate(qr, { 
@@ -14,14 +16,41 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-	console.log(message.body);
+    console.log(message.body);
 });
 
-client.on('message', message => {
-	if(message.body === 'Hello') {
-		//message.reply('World');
+/* client.on('message', message => {
+    if(message.body === 'Hello') {
+        //message.reply('World');
         client.sendMessage(message.from, 'World');
-	}
+        //client.sendMessage(media, {caption: 'Buenos días'});
+	} else {
+        client.sendMessage(message.from, "No tengo respuesta para eso!!");
+    }
+}); */
+
+client.on('message', async (msg) => {
+    
+    const chat = await msg.getChat();
+    const contact = await msg.getContact();
+    chat.sendMessage(contact);
+    await chat.sendMessage(media, {caption: `Hello @${contact.id.user}`}, {
+        mentions: [contact]
+    });
+    
+    await chat.sendMessage(`Hello @${contact.id.user}`, {
+        mentions: [contact]
+    });
+
+    /* if(message.body === 'Hello') {
+        //message.reply('World');
+        client.sendMessage(message.from, 'World');
+        client.sendMessage(`${contact.pushname} was mentioned`);
+        //client.sendMessage(media, {caption: 'Buenos días'});
+	} else {
+        client.sendMessage(message.from, "No tengo respuesta para eso!!");
+    } */
+
 });
 
 
